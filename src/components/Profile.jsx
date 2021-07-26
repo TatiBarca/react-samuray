23d6621ab9defa482500c3b29d76084f88cf8b98
TatiBarca <tatiana160787@gmail.com>
2021-07-26 16:23:52 +0300

    git-repositori

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <img src="https://img5.goodfon.ru/wallpaper/nbig/a/8f/piony-rozovyi-tekstura.jpg" />
+
+        <div>
+            avaDescription
+        </div>
+        <div>
+            my post
+        </div>
+        <div>
+            new post
+        </div>
+        <div>
+            post1
+        </div>
+        <div>
+            post2
+
+        </div>
+
+    </div>
+};
+
+export default Profile;
