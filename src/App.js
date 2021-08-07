522a9094e859ef4ede40587a3d2e14f4d4862afa
TatiBarca <tatiana160787@gmail.com>
2021-08-07 22:08:30 +0300

    changed a href on NavLink to for link in Navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { BrowserRouter, Route } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
 
 const App = () => {
 
@@ -16,6 +20,10 @@ const App = () => {
         <div className='app_wrapper_content'>
           <Route path='/dialogs' component={Dialogs} />
           <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+
 
         </div>
       </div>
